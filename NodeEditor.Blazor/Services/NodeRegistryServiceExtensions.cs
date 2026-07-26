f8fe111ce3377869ceff801b57b7039500b3512d
Craig Russell <tenowg@gmail.com>
2026-07-26 12:49:35 -0400

    Support multiple keyed INodeRegistryService instances
    Enable NodeEditorCanvas to resolve and provide keyed INodeRegistryService via DI and CascadingValue. Update ContextMenu to use cascaded registry if present. Add NodeRegistryServiceExtensions with AddKeyedNodeRegistry for flexible registration. Apply minor formatting and injection updates.

---
+++ NodeEditor.Blazor/Services/NodeRegistryServiceExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NodeEditor.Net.Services.Registry;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeEditor.Blazor.Services
+{
+    public static class NodeRegistryServiceExtensions
+    {
+        extension(IServiceCollection services) 
+        {
+            public IServiceCollection AddKeyedNodeRegistry(
+                object key,
+                ServiceLifetime lifetime,
+                Action<INodeRegistryService> configure)
+            {
+                services.Add(new ServiceDescriptor(
+                    serviceType: typeof(INodeRegistryService),
+                    serviceKey: key,
+                    factory: (serviceProvider, _) =>
+                    {
+                        // DI supplies all constructor dependencies of NodeRegistryService.
+                        //var registry = ActivatorUtilities.CreateInstance<NodeRegistryService>(
+                        //    serviceProvider);
+
+                        //// Registration occurs once when this keyed instance is created.
+                        //configure(registry);
+                        var registry = new NodeRegistryService(new NodeDiscoveryService());
+                        configure(registry);
+                        //registry.RegisterFromAssembly(AppDomain.CurrentDomain.GetAssemblies()[0]);
+                        return registry;
+                    },
+                    lifetime: lifetime));
+
+                return services;
+            }
+        }
+    }
+}
